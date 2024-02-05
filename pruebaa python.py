cff3da724fafab6690ec09abf4c588c024b6fddd
razovica <razovica@gmail.com>
2024-02-05 18:10:57 -0300

    Add files via upload

---
+++ pruebaa python.py
@@ -0,0 +1,11 @@
+# variables
+# tipos de datos
+# work.rvillasmil@gmail.com
+# reto # 1
+name     = input ('Ingresa tu Nombre:')
+Apellido = input ('Ingresa tu Apellido:')
+Movie    = input ('Ingresa tu Numero de Nombre:')
+Email    = input ('Ingresa tu Email:')
+print ("Hola " + name + " " + Apellido +", en breve recibiras un correo a " + Email)
+
+
